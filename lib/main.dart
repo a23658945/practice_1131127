48cfb3552f1f0525dcf0c170478c7153dbfcb661
Adam <adama23658945@gmail.com>
2024-11-27 11:55:33 +0800

    finish-1131127

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,15 @@ class _MyWidgetState extends State<MyWidget> {
   int count = 0;
   int count0 = 0;
   bool _check = false;
+  double _value = 0.0;
+  
   Color checkedColor = Colors.yellow;
+
+  void _onChange(e){
+    setState(() {
+      _value = e;
+    });  
+  }
 
   void increase() {
     if (count0 >= 25) {
@@ -40,45 +48,83 @@ class _MyWidgetState extends State<MyWidget> {
       count0 += 3;
       _check = false;
       checkedColor = Colors.purple;
-    };
+    }    
   }
+  
 
-    @override
-    Widget build(BuildContext context) {
-      return Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "Hello World! + $count",
-              style: TextStyle(fontSize: 30),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    //setState重新喧染函示
-                    count += 1;
-                    if (count == 10) {
-                      count = 0;
-                    }
-                  });
-                },
-                child: Text("按我+1 count=$count")),
-            SizedBox(
-              height: 10,
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  setState(() => increase());
-                },
-                child: Text("按我+1 count0=$count0")),
-            Checkbox(value: _check, onChanged: (bool? value) => setState(() => _check = value??false),checkColor: Colors.white, activeColor: checkedColor,)
-          ],
-        ),
-      );
-    }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "Hello World! + $count",
+            style: TextStyle(fontSize: 30),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  //setState重新喧染函示
+                  count += 1;
+                  if (count == 10) {
+                    count = 0;
+                  }
+                });
+              },
+              child: Text("按我+1 count=$count")),
+          SizedBox(
+            height: 10,
+          ),
+          ElevatedButton(
+              onPressed: () {
+                setState(() => increase());
+              },
+              child: Text("按我+1 count0=$count0")),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Checkbox(
+                value: _check,
+                onChanged: (bool? value) =>
+                    setState(() => _check = value ?? false),
+                checkColor: Colors.white,
+                activeColor: checkedColor,
+              ),
+              Container(
+                  width: 100,
+                  child: _check
+                      ? Image.asset(
+                          "assets/light-bulb_close_0.png") //_check bool為false
+                      : Image.asset(
+                          "assets/light-bulb_open_0.png") //_check bool為true
+                  )
+            ],
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Container(
+            width: 100,
+            height: _value,
+            color: Colors.red,
+          ),
+          Text('當前值:${_value.toStringAsFixed(1)}'
+          ),
+          Slider(
+            value: _value,
+            min: 0.0,
+            max: 360.0,
+            activeColor: Colors.orangeAccent,
+            inactiveColor: Colors.green.withAlpha(99),
+            onChanged: _onChange),
+        ],
+      ),
+    );
   }
+}
 
